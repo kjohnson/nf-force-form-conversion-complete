1215ccc36fa08919830c65e3a665f7b4c3ef9d7c
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2015-08-10 12:24:49 -0400

    Create nf-form-conversion-complete.php

---
+++ nf-form-conversion-complete.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/*
+ * Plugin Name: NF Utility - Force Form Conversion Complete
+ * Plugin URI: http://kylebjohnson.me
+ * Description: Manually sets the form conversion flags.
+ * Version: 0.1.0
+ * Author: Kyle B. Johnson
+ */
+
+add_action( 'init', 'kbj_nf_force_form_conversion_complete' );
+function kbj_nf_force_form_conversion_complete(){
+
+    // Manually set flags from the Form Conversion Upgrade
+    update_option( 'nf_convert_forms_complete', TRUE );
+    update_option( 'nf_converted_form_reset', FALSE );
+
+    // Self Deactivate
+    deactivate_plugins( plugin_basename( __FILE__ ) );
+}
